e6827ff657b13207bf5accf479fb7122b9a192f3
Ostap <ostapmedunskij@gmail.com>
2020-11-18 15:30:05 +0200

    setup client-side && config auth on back && auth middleware

--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
-const db = require('./keys').mongoURI
+const config = require('config')
+const db = config.get('mongoURI')
 
 const connectDB = async () => {
 	try {
--- routes/api/events.js
+++ routes/api/events.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-
+const auth = require('../../middleware/auth')
 // Event Model
 const Event = require('../../models/Event')
 
