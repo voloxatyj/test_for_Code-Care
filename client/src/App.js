1a810ee902043ff23647738fcca86f4933d5400c
Ostap <ostapmedunskij@gmail.com>
2020-11-18 17:29:31 +0200

    add SignIn&SignUp pages fro client side && actions

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,36 @@
 import './App.css'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 // import jwtDecode from 'jwt-decode'
 import { SET_AUTH, SET_USER } from './redux/types'
 
 // Redux Init
 import { Provider } from 'react-redux'
 import { store } from './redux/store'
+import { SignIn } from './components/SignIn'
+import { SignUp } from './components/SignUp'
 
-// const token = localStorage.getItem('token') || undefined
-// const user = JSON.parse(localStorage.getItem('userData'))
+const token = localStorage.getItem('token') || undefined
+const user = JSON.parse(localStorage.getItem('userData'))
 
-// if(token){
-//   const decodedToken = jwtDecode(token)
-//   if(decodedToken.exp * 1000 < Date.now()){
-//     localStorage.removeItem('token')
-//     window.location.href='/signin'
-//   }
-//   store.dispatch({ type: SET_AUTH })
-//   store.dispatch({type: SET_USER,payload: user})
-// }
+if(token){
+  const decodedToken = jwtDecode(token)
+  if(decodedToken.exp * 1000 < Date.now()){
+    localStorage.removeItem('token')
+    window.location.href='/signin'
+  }
+  store.dispatch({ type: SET_AUTH })
+  store.dispatch({type: SET_USER,payload: user})
+}
 
 export const App = () => {
   return (
     <Router>
       <Provider store={store}>
         <div className="container">
-          <h1>Hello</h1>
+          <Switch>
+            <Route exact path='/signin' component={SignIn} />
+            <Route exact path='/signup' component={SignUp} />
+          </Switch>
         </div>
       </Provider>
     </Router>
